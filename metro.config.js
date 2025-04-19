9e4ab819a567adc3b3ed9124fd9eccd88e7d4046
sirgalahad0529 <allan.lucero29@gmail.com>
2025-04-19 10:29:54 +0800

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,20 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Exclude duplicate package.json files that are causing Haste module naming collision
+config.resolver.blockList = [
+  /mobile-app-final7\/package\.json$/,
+  /mobile-app-final\d*\//,
+  /mobile-app-simple\//,
+  /mobile-app-eas-build\//,
+  /mobile-app-v5\//,
+  /complete-app\//,
+  /final-app\//,
+  /extracted\//,
+];
 
 // Remove all console statements except errors in production
 config.transformer.minifierConfig = {
